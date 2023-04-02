92462841951b7c6602f5425a5ad04d3e4c978c6c
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-04-03 04:39:50 +0600

    styling cart

--- hooks/useCart.js
+++ hooks/useCart.js
@@ -1,7 +1,11 @@
 import products from "@/data/products";
+import { getProductsFromCart } from "@/utils/cartManager";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 // write a simple hook that returns a list of products
+
+import styles from '../styles/ShoppingList.module.css'
+
 
 
 export const useCart = () => {
@@ -13,9 +17,10 @@ export const useCart = () => {
     };
 
     useEffect(() => {
-        setProducts(products)
+        let cartProducts = getProductsFromCart();
+        setProducts(cartProducts)
     }, []);
-    
+
     return {
         products: loadedProducts,
         gotoCart: gotoCartPage
--- pages/cart.js
+++ pages/cart.js
@@ -1,8 +1,72 @@
-import React from 'react'
+import Navigation from '@/components/navigation/navigation';
+import { useCart } from '@/hooks/useCart';
+import { decreaseQuantityOfProductInCart, increaseQuantityOfProductInCart } from '@/utils/cartManager';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react'
+
+import styles from '../styles/Cart.module.css'
+
+
 
 const Cart = () => {
+
+
+    const { products, gotoCart } = useCart();
+    const router = useRouter();
+
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+
+
+    if (isNavOpen) {
+        return (
+            <Navigation
+                setIsNavOpen={setIsNavOpen}
+            />
+        )
+    }
+
+
     return (
-        <div>Cart</div>
+        <div className={styles.page}>
+
+            <div className={styles.topBar}>
+                <div className={styles.hamburger} onClick={() => { setIsNavOpen(true) }} />
+                <div className={styles.pageTitle}> Cart </div>
+                <div className={styles.cart} onClick={gotoCart} />
+            </div>
+
+            <div className={styles.products}>
+                {
+                    products.length === 0 && 
+                    <div className={styles.emptyCart}>
+                        <div className={styles.emptyCartText}>Your cart is empty</div>
+                    </div>
+                }
+                {
+                    products.map((product) => (
+                        <div className={styles.product} key={product.id}>
+                            <div className={styles.productInformation}>
+                                <div className={styles.productImage}>
+                                    <img src={product.image} alt={product.name} />
+                                </div>
+                                <div className={styles.productName}>{product.name}</div>
+                                <div className={styles.productPrice}>${product.price}</div>
+
+
+                                <div> x {product.quantity}</div>
+                            </div>
+                            <div className={styles.productActionButtons}>
+                                <div className={styles.removeProduct} onClick={() => { decreaseQuantityOfProductInCart(product); router.reload() }} />
+                                <div className={styles.productQuantity}>${product.quantity}</div>
+                                <div className={styles.addProduct} onClick={() => { increaseQuantityOfProductInCart(product); router.reload() }} />
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+
+        </div>
     )
 }
 
