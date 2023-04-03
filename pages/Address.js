9908ad6f2688eb338db729d6e0461d38696cf8f8
Jeesan57 <115409557+Jeesan57@users.noreply.github.com>
2023-04-03 15:33:22 +0600

    Address page structure

---
+++ pages/Address.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from '../styles/Address.module.css'
+const Address = () => {
+
+  
+  return (
+    <div className={styles.topBar}>
+      <div className={styles.hamburger}/>
+      <div className={styles.pageTitle}> Address </div>
+      <div className={styles.cart} />
+    </div>
+    
+  
+  )
+}
+
+export default Address
